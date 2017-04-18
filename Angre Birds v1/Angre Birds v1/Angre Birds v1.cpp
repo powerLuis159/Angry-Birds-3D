734a5c4cd1b5224cc120744acdf20976f1af3dd3
Luis Ronald Istaña Chipana <l.ronald.ic@outlook.com>
2017-04-17 19:11:02 -0500

    opengl_props agregado

--- Angre Birds v1/Angre Birds v1/Angre Birds v1.cpp
+++ Angre Birds v1/Angre Birds v1/Angre Birds v1.cpp
@@ -56,7 +56,7 @@ int main()
 
 	// Camera matrix
 	glm::mat4 View = glm::lookAt(
-		glm::vec3(0, -50, 10), // Camera is at (4,3,3), in World Space
+		glm::vec3(0, -50, 10), // Camera is at (X,Y,Z), in World Space
 		glm::vec3(0, 0, 0), // and looks at the origin
 		glm::vec3(0, 1, 0)  // Head is up (set to 0,-1,0 to look upside-down)
 	);
